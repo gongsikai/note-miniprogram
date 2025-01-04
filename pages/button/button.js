0889e27d4ac18e6d380212e99386a94461f93c20
dev.git.0 <dev.git.0@euse.site>
2025-01-05 00:39:52 +0800

    add npm md5 package and add npm tdesign-miniprogram packages and add login page

--- pages/button/button.js
+++ pages/button/button.js
@@ -1,1 +1,24 @@
-Page({});
+const md5 = require('md5')
+
+Page({
+  onLoad() {
+    // wx.request({  })
+    wx.request({
+      url: 'https://api.note.gongsikai.com/api/user/login', //仅为示例，并非真实的接口地址
+      method: 'POST',
+      data: {
+        name: '1',
+        pass: md5('1')
+      },
+      header: {
+        'content-type': 'application/json', // 默认值
+        token: wx.getStorageSync('token')
+      },
+      success (res) {
+        console.log('res.data', res.data)
+        wx.setStorageSync('token', res.data.data.token)
+        console.log('token', wx.getStorageSync('token'))
+      }
+    })
+  }
+});
