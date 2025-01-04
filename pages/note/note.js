fe782a6f22b6b02c7574df3a306e36044e210e7c
dev.git.0 <dev.git.0@euse.site>
2025-01-05 04:24:30 +0800

    add noteEdit

--- pages/note/note.js
+++ pages/note/note.js
@@ -28,7 +28,8 @@ Page({
     this.login()
     console.log('event', event)
   },
-  bindTabLogout(event) {
+  // bindTabLogout(event) {
+  bindTapLogout(event) {
     this.logout()
     console.log('event', event)
   },
@@ -37,12 +38,29 @@ Page({
   //   this.noteSave()
   //   console.log('event', event)
   // },
-  bindTabSave(event) {
+  // bindTabSave(event) {
+  bindTapSave(event) {
     // this.save()
     this.noteSave()
     console.log('event', event)
   },
-  bindTabDelete(event) {
+  bindTapEdit(event) {
+    // this.save()
+    this.noteEdit(event)
+    console.log('event', event)
+  },
+  bindTapEditCancel(event) {
+    // this.save()
+    this.noteEditCancel(event)
+    console.log('event', event)
+  },
+  bindTapEditSave(event) {
+    // this.save()
+    this.noteEditSave(event)
+    console.log('event', event)
+  },
+  // bindTabDelete(event) {
+  bindTapDelete(event) {
     // this.delete()
     this.noteDelete(event)
     console.log('event', event)
@@ -165,6 +183,98 @@ Page({
       }
     })
   },
+  noteEdit(event) {
+    const { index } = event.currentTarget.dataset
+    this.setData({
+      // noteContent: '',
+      noteList: this.data.noteList.map((item, noteIndex) => noteIndex === index ? ({
+        ...item,
+        editable: true
+      }) : item)
+    })
+  },
+  noteEditCancel(event) {
+    const { index } = event.currentTarget.dataset
+    this.setData({
+      // noteContent: '',
+      noteList: this.data.noteList.map((item, noteIndex) => noteIndex === index ? ({
+        ...item,
+        editable: false
+      }) : item)
+    })
+  },
+  bindInputTextarea(event) {
+    const { index } = event.currentTarget.dataset
+    const { value } = event.detail
+    console.log('value', value)
+    this.setData({
+      // noteContent: '',
+      noteList: this.data.noteList.map((item, noteIndex) => noteIndex === index ? ({
+        ...item,
+        editvalue: value,
+      }) : item)
+    })
+  },
+  noteEditSave(event) {
+    // const { content } = event.currentTarget.dataset
+    const { editvalue } = event.currentTarget.dataset
+    console.log('event', event)
+    console.log('content', event.currentTarget.dataset)
+    const that = this;
+    wx.request({
+      url: 'https://api.note.gongsikai.com/api/note/add', //仅为示例，并非真实的接口地址
+      // method: 'POST',
+      // method: 'GET',
+      method: 'POST',
+      // data: {
+      //   // name: '1',
+      //   name: this.data.name,
+      //   // pass: md5('1'),
+      //   pass: md5(this.data.pass)
+      // },
+      data: {
+        // uuid: 'uuid',
+        // content: encodeURIComponent(contentTextarea),
+        // content: encodeURIComponent(editvalue),
+        content: editvalue,
+      },
+      header: {
+        'content-type': 'application/json', // 默认值
+        token: wx.getStorageSync('token')
+      },
+      success (res) {
+        console.log('res.data', res.data)
+        that.setData({
+          // noteContent: '',
+          noteList: that.data.noteList.map((item) => ({
+            ...item,
+            editable: false
+          }))
+        })
+        that.methodsNoteList()
+        // wx.setStorageSync('token', res.data.data.token)
+        // this.setData({
+        //   token: res.data.data.token
+        // })
+        // that.setData({
+        //   // noteList: res.data.data.token,
+        //   // noteList: res.data.data.map(item => ({ ...item, content: decodeURIComponent(item.content) })),
+        //   // noteList: res.data.data.map(item => ({ 
+        //   //   ...item,
+        //   //   content: decodeURIComponent(item.content),
+        //   //   create_time: moment(item.create_time).parse('YYYY DD MM HH:mm:SS')
+        //   // })),
+        //   noteList: res.data.data.map(item => ({ 
+        //     ...item,
+        //     content: decodeURIComponent(item.content),
+        //     create_time: moment(item.create_time).format('YYYY年MM月DD日 hh时mm分ss秒'),
+        //     eidtable: false
+        //   }))
+        // })
+        // console.log('token', wx.getStorageSync('token'))
+      }
+    })
+  },
   noteDelete(event) {
     console.log('event', event)
     const that = this;
@@ -247,7 +357,10 @@ Page({
             ...item,
             content: decodeURIComponent(item.content),
             create_time: moment(item.create_time).format('YYYY年MM月DD日 hh时mm分ss秒'),
-            eidtable: false
+            // eidtable: false,
+            editable: false,
+            editvalue: decodeURIComponent(item.content),
+            create_time: moment(item.create_time).format('YYYY年MM月DD日 hh时mm分ss秒'),
           }))
         })
         // console.log('token', wx.getStorageSync('token'))
